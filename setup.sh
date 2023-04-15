43feababe9031e8961c5acd286f0b59fe6fb7399
MaksKhan <72515541+MaksKhan@users.noreply.github.com>
2023-04-15 17:20:27 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+!sudo apt install libcudnn8 libcudnn8-dev libnccl-dev libnccl2 --allow-change-held-packages
+!pip install transformers
+!pip install wandb 
+!pip install bitsandbytes
+!pip install pytorch_lightning==1.9.4
